53df2e4a626aae2612bdfd990e3d67bed360ecff
Jonathan Thomas <jonathanthomas@172-18-12-226.DYNAPOOL.NYU.EDU>
2017-08-07 19:59:08 -0400

    added test script

---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export SPOTIPY_CLIENT_ID='30e01cfb0e94403f8ac3799b6a0d4171'
+export SPOTIPY_CLIENT_SECRET='da24db4df0ea4b7db8a7217adb6ef269'
+export SPOTIPY_REDIRECT_URI='http://jonathoma.github.io/songoftheday/'
